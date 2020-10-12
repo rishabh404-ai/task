801a7247d2a0187decc78b2ed20291d68e8aec8e
Rishabh <mishrarishabh404it@gmail.com>
2020-10-12 15:04:03 +0530

    Added Admin Configuratios

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Register
 
-# Register your models here.
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ('name','idcard_no','id_type','address','phone_no','email','meet_with',)
+    list_filter = ('name','idcard_no','id_type','address','phone_no','email','meet_with',)
+    search_fields = ('name','idcard_no','id_type','address','phone_no','email','meet_with',)
+
+admin.site.register(Register)    
