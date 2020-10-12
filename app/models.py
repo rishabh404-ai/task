8eb0def92c27a466071f31114da82cc6ee5a7149
Rishabh <mishrarishabh404it@gmail.com>
2020-10-12 14:06:31 +0530

    Added Register Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+ID_CHOICES =( 
+        ("1", "PAN"), 
+        ("2", "Aadhar"), 
+        ("3", "VoterID"), 
+        ("4", "Others"), 
+    ) 
+
+class Register(models.Model):
+    name = models.CharField(max_length=100)
+    idcard_no = models.IntegerField()
+    id_type = models.CharField(max_length=1,choices=ID_CHOICES)
+    address = models.TextField()
+    phone_no = models.CharField(max_length=11)
+    email = models.EmailField()
+    meet_with = models.CharField(max_length=100)
+    
+
+
+
